44593bcf97a7e1e33d361eac3a3e7eb5e5ecc41e
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-06 00:18:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(arr)
+  return arr.all? { |word| word[0] == 'r'}
+end
+
+def contain_a(arr)
+  return arr.select{ |word| word.include?('a')}
+end
+
+def first_wa(arr)
+  return arr.find{ |el| el.to_s.include?('wa')}
+end
+
+def remove_non_strings(arr)
+  return arr.delete_if{ |e| !e.kind_of?(String)}
+end
+
+def count_elements(arr)
+  arr.group_by(&:itself).collect do |key, value|
+     key.merge({:count => value.length})
+   end
+end
+
+def merge_data()
+end
+
+def find_cool(arr)
+  cools = arr.collect do |hash|
+     hash if hash.has_value?('cool')
+  end
+  cools.compact
+end
+
+def organize_schools(arr)
+  binding.pry
+end
